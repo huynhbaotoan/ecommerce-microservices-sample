e0fbce9e20e508596ac19920d790ca371bee6c21
Toan Huynh <tbhuynh@amazon.com>
2021-11-27 00:46:34 +0700

    update to delete booking as create order

--- awsomepets-serverless/src/order/UpdateInventoryFunction/index.js
+++ awsomepets-serverless/src/order/UpdateInventoryFunction/index.js
@@ -1,6 +1,7 @@
 const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
-const inventoryTable = process.env.CATALOGUE_TABLE;
+const catalogueTable = process.env.CATALOGUE_TABLE;
+const inventoryTable = process.env.INVENTORY_TABLE;
 
 class ErrUpdateInventory extends Error {
   constructor(message) {
@@ -16,12 +17,13 @@ exports.handler = async (event, context) => {
   // Update inventory and save to catalog table
   let orderItems = [];
   let transactionDate = Date.now();
+  let bookingItems = [];
   for(let i=0;i<order.basket.length; i++) {
     let item = order.basket[i];
 
     orderItems.push({
       Update: {
-        TableName: inventoryTable,
+        TableName: catalogueTable,
           Key:{
             "id": item.id
           },
@@ -36,6 +38,17 @@ exports.handler = async (event, context) => {
           ReturnValuesOnConditionCheckFailure: "ALL_OLD"
       }
     });
+    
+    bookingItems.push({
+      Delete: {
+        TableName: inventoryTable,
+        Key:{
+          "orderId": event.orderId,
+          "sk": item.id
+        },
+        ReturnValuesOnConditionCheckFailure: "NONE"
+      }
+    });
   }
 
   let params = {
@@ -43,7 +56,7 @@ exports.handler = async (event, context) => {
     /*[
       {
         ConditionCheck: {
-          TableName: inventoryTable,
+          TableName: catalogueTable,
           Key:{
             "id": "81ad9dae-3f63-4e64-baac-a984dfc2fb28"
           },
@@ -63,6 +76,13 @@ exports.handler = async (event, context) => {
   // Put in database
   try {
     await ddb.transactWrite(params).promise();
+
+    //delete all bookings
+    let bookingParams = {
+      TransactItems: bookingItems
+    };
+    await ddb.transactWrite(bookingParams).promise();
+    
   } catch(e) {
     console.log(e);
     throw new ErrUpdateInventory("Error updating inventory")
